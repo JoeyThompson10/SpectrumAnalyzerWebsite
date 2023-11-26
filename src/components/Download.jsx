7850f4e462f3302e36ba7926ac325b6f42360a62
JoeyThompson10 <108598229+JoeyThompson10@users.noreply.github.com>
2023-11-26 09:34:41 -0500

    Removes installer link

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -9,7 +9,7 @@ const About = () => {
             <h2>Installation Instructions:</h2>
 
             <h3>GitHub Repository:</h3>
-            <p>You can also download and run the project from our GitHub repository. Here's how:</p>
+            <p>Download and run the project from our GitHub repository. Here's how:</p>
             <ol>
                 <li>Visit our GitHub repository at <a href="https://github.com/JoeyThompson10/spectrumAnalyzerProject" target="_blank">Spectrum Analyzer Project</a>.</li>
                 <li>Clone the repository or download the ZIP file and extract it on your computer.</li>
@@ -18,9 +18,9 @@ const About = () => {
                 <li>Ensure you have Python installed on your system and the necessary libraries by running <code>pip install -r requirements.txt</code> (the requirements file should list all the necessary libraries).</li>
                 <li>Run the application by executing <code>python main.py</code> in the terminal.</li>
             </ol>
-            <p>Note: You can find detailed instructions and documentation on how to set up and run the project in the repository's README file.</p>
+            <p>Note: You can also find detailed instructions and documentation on how to set up and run the project in the repository's README file.</p>
 
-            <h3>Installer:</h3>
+            {/* <h3>Installer:</h3>
             <p>Download the installer and run it on your system. Follow the on-screen instructions to complete the installation.</p>
 
             <a href={appInstallerUrl} download>
@@ -29,7 +29,7 @@ const About = () => {
             <p>Version: 1.6</p>
             <p>File Size: 15 MB</p>
             <p>Release Date: 2023-11-05</p>
-            <p>System Requirements: Windows 10/11, 4GB RAM, 500MB Disk Space</p>
+            <p>System Requirements: Windows 10/11, 4GB RAM, 500MB Disk Space</p> */}
         </div>
     );
 };
